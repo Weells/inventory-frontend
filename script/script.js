b79fa18378ca507ec00f8b35316c305933b84ec7
weells <inthemiddleh@gmail.com>
2023-09-24 19:12:55 -0300

    Modificações de responsividade e melhorias nos cards

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 const ingredientsButton = document.getElementById("ingredientsButton");
 const inputContent = document.getElementById("search-input");
+const cardConfig = document.querySelector('.menu');
 
 function getIngredients() {
     fetch("http://localhost:8080/ingredients")
@@ -7,21 +8,13 @@ function getIngredients() {
         .then(data => {
             console.log(data);
             const cardsSection = document.querySelector('.cards-section');
+            const label = document.querySelector('#main-section-label h1');
+            const labelIcon = document.querySelector('#main-section-label img');
             cardsSection.innerHTML = "";
+            label.textContent = "Ingredientes";
+            labelIcon.src = "./images/icons8-atom-50.png"
             data.forEach(element => {
                 createCard(element);
-                // const div = document.createElement('div');
-                // div.className = 'card';
-                // div.classList.add('fade-in');
-
-                // div.innerHTML = `
-                // <img src="./images/${element.icon}-icon.png">
-                // <h1>${element.ingredientName}</h1>
-                // <p class="card-description">Lorem ipsum dolor sit amet</p>
-                // <p class="card-quantity">Qtd: ${element.quantity}</p>
-                // <a href="#">Ler mais</a>`;
-
-                // cardsSection.appendChild(div);
             });
         })
 }
@@ -38,18 +31,7 @@ function getRecipes() {
             label.textContent = "Receitas";
             labelIcon.src = './images/book-icon (1).png';
             data.forEach(element => {
-                createRecipeCard(element);
-                // div.className = 'card';
-                // div.classList.add('fade-in');
-
-                // div.innerHTML = `
-                // <img src="./images/${element.icon}-icon.png">
-                // <h1>${element.recipeName}</h1>
-                // <p class="card-description">Uma chávena ou xícara (escrito chícara em português de Portugal) é um recipiente ou utensílio doméstico, usado para bebidas quentes ou frias. O termo xícara, usado no Brasil na atualidade, caiu em desuso em Portugal, surgindo apenas na literatura e em romances do século XIX.</p>
-                // <p class="card-quantity">Qtd: ${element.quantity}</p>
-                // <a href="#">Ler mais</a>`;
-
-                //cardsSection.appendChild(div);
+                createCard(element);
             });
         })
 }
@@ -63,37 +45,59 @@ function getIngredientById() {
         })
 }
 
+// function createCard(data) {
+//     const cardsSection = document.querySelector('.cards-section');
+
+//     const newCard = document.createElement('div');
+//     newCard.className = 'card';
+//     newCard.id = data.id;
+//     newCard.classList.add('fade-in');
+
+//     newCard.innerHTML = `
+//         <div class="menu">
+//             <span class="bar"></span>
+//             <span class="bar"></span>
+//             <span class="bar"></span>
+//         </div>
+//         <img src="./images/${data.icon}-icon.png">
+//         <h1>${data.ingredientName}</h1>
+//         <p class="card-description">${data.description}</p>
+//         <p class="card-quantity">Qtd: ${data.quantity}</p>
+//         <a href="#">Ler mais</a>`;
+
+//     cardsSection.appendChild(newCard);
+// }
+
 function createCard(data) {
     const cardsSection = document.querySelector('.cards-section');
-
     const newCard = document.createElement('div');
     newCard.className = 'card';
+    newCard.id = data.id;
     newCard.classList.add('fade-in');
+    
+    let objectName;
+    if("recipeName" in data) {
+        objectName = data.recipeName;
+    }
+    if("ingredientName" in data) {
+        objectName = data.ingredientName;
+    }
 
     newCard.innerHTML = `
-        <img class="card-config" src="./images/ellipsis-vertical-solid.svg" width="15" height="15" alt="">
+        <div class="menu">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
         <img src="./images/${data.icon}-icon.png">
-        <h1>${data.ingredientName}</h1>
-        <p class="card-description">Lorem ipsum dolor sit amet</p>
+        <h1>${objectName}</h1>
+        <p class="card-description">${data.description}</p>
         <p class="card-quantity">Qtd: ${data.quantity}</p>
         <a href="#">Ler mais</a>`;
-
-    cardsSection.appendChild(newCard);
-}
-function createRecipeCard(data) {
-    const cardsSection = document.querySelector('.cards-section');
-
-    const newCard = document.createElement('div');
-    newCard.className = 'card';
-    newCard.classList.add('fade-in');
-
-    newCard.innerHTML = `
-        <img class="card-config" src="./images/ellipsis-vertical-solid.svg" width="15" height="15" alt="">
-        <img src="./images/${data.icon}-icon.png">
-        <h1>${data.recipeName}</h1>
-        <p class="card-description">Lorem ipsum dolor sit amet</p>
-        <p class="card-quantity">Qtd: ${data.quantity}</p>
-        <a href="#">Ler mais</a>`;
-
+    
+    const cardConfig = newCard.querySelector('.menu');
+    cardConfig.onclick = () => {
+        cardConfig.classList.toggle('active');
+    }
     cardsSection.appendChild(newCard);
 }
